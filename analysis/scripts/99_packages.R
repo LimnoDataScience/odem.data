fca3b14ab227a456cb6dfde74627bae831c5dcf9
Robert <ladwigjena@gmail.com>
2021-01-21 14:38:33 -0600

    updated everything into 01_data_merge_sql.R which grabs data and runs updated ODEM model

--- analysis/scripts/99_packages.R
+++ analysis/scripts/99_packages.R
@@ -2,6 +2,12 @@ suppressMessages(library(glmtools))
 suppressMessages(library(dplyr))
 suppressMessages(library(lubridate))
 suppressMessages(library(zoo))
+suppressMessages(library(DBI))
+suppressMessages(library(RPostgreSQL))
+suppressMessages(library(LakeMetabolizer))
+suppressMessages(library(pracma))
+suppressMessages(library(ggplot2))
+
 
 library(odem.data)
 
