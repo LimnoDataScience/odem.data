3e09bed45c7670a1323f57a779d43a9eb334acb1
Robert <ladwigjena@gmail.com>
2021-01-22 09:55:31 -0600

    delete

--- R/test.R
+++
@@ -1,6 +0,0 @@
-remotes::install_github("robertladwig/odem.data")
-
-library(odem.data)
-lake_id <- 'nhdhr_120018114' #"analysis/data/143249470_Mendota/"
-***REMOVED***
-source("analysis/scripts/01_data_merge_sql.R")
